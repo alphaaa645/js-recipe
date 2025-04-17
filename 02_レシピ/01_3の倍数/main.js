a4218f01ca9ac00fb15d96a25bee0caf5988472c
alphaaa645 <qingshuizezou@gmail.com>
2025-04-17 12:47:15 +0900

    [3の倍数]を学習しました。

---
+++ 02_レシピ/01_3の倍数/main.js
@@ -0,0 +1,10 @@
+const genkiCounter = (number) => {
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 === 0) {
+      console.log(n + "!!!!!!!")
+    } else {
+      console.log(n)
+    }
+  }
+}
+genkiCounter(284)
