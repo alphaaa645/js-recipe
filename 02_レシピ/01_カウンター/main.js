83cbdc946a45a1ea21708e7948fe6002178c4040
alphaaa645 <qingshuizezou@gmail.com>
2025-04-18 14:27:18 +0900

    [カウンター(発展)]を学習しました

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,11 +1,20 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
-
+const minusButton = document.getElementById("minus-button")
+const doubleButton = document.getElementById("double-button")
 let count = 0
 //ボタンをクリックしたときの処理を登録（関数）
 plusButton.onclick = () => {
   //countを更新
   count += 1
   //countを表示
+  display.textContent = count
+}
+minusButton.onclick = () => {
+  count -= 1
+  display.textContent = count
+}
+doubleButton.onclick = () => {
+  count *= 2
   display.textContent = count
 }
