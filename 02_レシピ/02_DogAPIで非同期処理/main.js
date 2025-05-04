26f9fac22de0b76a1f3a63668509c6ed817c1ea1
alphaaa645 <qingshuizezou@gmail.com>
2025-05-04 16:45:01 +0900

    [ワンちゃん切り替えボタンの作成]を完了しました

--- 02_レシピ/02_DogAPIで非同期処理/main.js
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -1,11 +1,18 @@
 const imageElement = document.getElementById("dog-image")
+const changeButton = document.getElementById("change-button")
 
 //指定したサーバーにデータを取りに行く
-fetch("https://dog.ceo/api/breeds/image/random")
-  .then((res) => {
-    return res.json()
-  })
-  .then((data) => {
-    //messageはデータの画像URLを指す
-    imageElement.src = data.message
-  })
+const randomDogImage = () => {
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      //messageはデータの画像URLを指す
+      imageElement.src = data.message
+    })
+}
+
+changeButton.addEventListener("click", randomDogImage, false)
+
+window.onload = randomDogImage
